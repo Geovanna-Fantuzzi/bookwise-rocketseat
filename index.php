1c7f307259bd8f7e512e3d884df2d240849ef93d
Geovanna-Fantuzzi <geovanna.fantuzzi@gmail.com>
2026-07-27 17:02:15 -0300

    :sparkles: feat: Card de livro

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,8 +9,9 @@
 
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 </head>
+
 <body class="bg-stone-950 text-stone-400">
-    
+
     <header class="bg-indigo-900 border-b-2 border-b-stone-900 shadow-lg shadow-gray-900">
         <nav class="mx-auto max-w-screen-lg flex justify-between py-4">
             <div class="font-extrabold text-xl tracking-wider">Book Wise</div>
@@ -32,11 +34,28 @@
             <input type="text" name="pesquisa" id="pesquisa" class="border-2 border-stone-800 bg-stone-900 rounded-md text-sm focus:outline-none px-2 py-1 w-full" placeholder="Pesquise um livro...">
             <button type="submit" class="bg-stone-400 border-2 border-stone-800 rounded-md text-stone-950 p-1">🔎</button>
         </form>
-        
+
+        <section class="space-y-4">
+            <!-- Card Livro-->
+            <div class="w-1/3 p-2 rounded border-stone-800 border-2 bg-stone-900">
+                <div class="flex">
+                    <div class="w-1/3">imagem</div>
+                    <div>
+                        <div class="font-semibold">Titulo</div>
+                        <div class="text-xs italic">Autor</div>
+                        <div class="text-xs italic">⭐⭐⭐⭐⭐(3 Avaliações)</div>
+                    </div>
+
+                </div>
+                <div class="text-sm">Descrição</div>
+            </div>
+        </section>
+
     </main>
-    
+
     <footer>
 
     </footer>
 </body>
+
 </html>
